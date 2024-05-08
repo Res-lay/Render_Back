97e012bfa9a85484b15a4e5ad76544cade73048e
Vanya <82407533+Res-lay@users.noreply.github.com>
2024-05-08 19:36:05 +0300

    Add files via upload

---
+++ target/classes/static/data.sql
@@ -0,0 +1,9 @@
+insert into book(author, path, price, description, name, quantity) values
+('Erich Maria Remarque', '/images/cover4.jpg',55, '','The shelter of dreams', 100),
+('Oldos Haksli', '/images/cover5.jpg',55, '','Genius and goddess', 150),
+('Mikhail Afanasyevich Bulgakov', '/images/cover6.jpg',55, '',' The Master and Margarita', 100),
+('Erich Maria Remarque', '/images/cover7.jpg',55, '','Three comrades', 100),
+('Emily Brontë', '/images/cover8.jpg',55, '','Wuthering heights', 100),
+('Francis Burnett ', '/images/cover9.jpg',55, '','The Mysterious Garden', 100),
+('Colleen McCullough', '/images/cover10.jpg',55, '','The Thorn Birds', 100);
+
